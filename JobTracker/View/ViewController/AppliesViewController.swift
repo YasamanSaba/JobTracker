993b33b324967f8ab06be46c59feaf13aa1e4dfa
Sam <samyjava@live.com>
2020-06-10 10:31:57 +0430

    Add AppliesTableViewCell. Add Apply.storyboard. Add AppliesViewController.

---
+++ JobTracker/View/ViewController/AppliesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AppliesViewController.swift
+//  JobTracker
+//
+//  Created by Sam Javadizadeh on 6/10/20.
+//  Copyright © 2020 Dream Catcher. All rights reserved.
+//
+
+import UIKit
+
+class AppliesViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
