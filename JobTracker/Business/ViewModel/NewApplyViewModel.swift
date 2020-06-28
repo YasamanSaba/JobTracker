8b24ddf176f007980813c7760176a70ef3eac646
Sam <samyjava@live.com>
2020-06-28 10:13:44 +0430

    Add NewApplyViewModel.

---
+++ JobTracker/Business/ViewModel/NewApplyViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  NewApplyViewModeType.swift
+//  JobTracker
+//
+//  Created by Sam Javadizadeh on 6/27/20.
+//  Copyright © 2020 Dream Catcher. All rights reserved.
+//
+
+import UIKit
+
+class NewApplyViewModel {
+    
+    // MARK: - Properties
+    let countryService: CountryServiceType
+    
+    // MARK: - Initializer
+    init(countryService: CountryServiceType) {
+        self.countryService = countryService
+    }
+    
+    // MARK: - Functions
+    func configureCountry(pickerView: UIPickerView) {
+        
+    }
+}
+
+
+// MARK: - Extensions
+
